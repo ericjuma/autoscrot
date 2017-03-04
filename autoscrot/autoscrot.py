d57cbe7a513a5c52366445f5e9ae9f3ba55c7f79
Eric Juma <ericjuma@gmail.com>
2017-03-04 05:45:26 -0500

    Add more docstrings and comments

--- autoscrot/autoscrot.py
+++ autoscrot/autoscrot.py
@@ -1,10 +1,11 @@
+from uuid import uuid4
 import argparse
 import helpers
 import os
-from uuid import uuid4
 
 
 def real_real_path(path):
+    """Takes a path and returns the absolute path using expanduser and realpath"""
     return os.path.realpath(os.path.expanduser(path))
 
 
@@ -58,24 +59,30 @@ def used_record_dir(path):
 
 
 def record(args):
+    """Called by argparse to run helper function with arguments from args namespace"""
     helpers.record(record_dir=args.record_dir, speed=args.speed)
 
 
 def export(args):
+    """Called by argparse to run helper function with arguments from args namespace"""
     helpers.export(record_dir=args.record_dir, output_file=args.output_file)
 
 
 def clear(args):
+    """Called by argparse to run helper function with arguments from args namespace"""
     helpers.clear(record_dir=args.record_dir)
 
 
 def status(args):
+    """Called by argparse to run helper function with arguments from args namespace"""
     helpers.status(record_dir=args.record_dir)
 
 
+# Create arg parser
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
 
+# Create subparser for record subcommand
 record_subparser = subparsers.add_parser(
     'record',
     help='Record screenshots to be exported. Appends to previous recordings.')
@@ -91,6 +98,7 @@ record_subparser.add_argument(
     help='Path to recording directory to record in')
 record_subparser.set_defaults(func=record)
 
+# Create subparser for export subcommand
 export_subparser = subparsers.add_parser(
     'export',
     help='Export recorded data to video file.')
@@ -106,7 +114,7 @@ export_subparser.add_argument(
     help='Path to recording directory to export from')
 export_subparser.set_defaults(func=export)
 
-
+# Create subparser for status subcommand
 status_subparser = subparsers.add_parser(
     'status',
     help='Get info on the recorded data.')
@@ -117,6 +125,7 @@ status_subparser.add_argument(
     help='Path to recording directory to get info on')
 status_subparser.set_defaults(func=status)
 
+# Create subparser for clear subcommand
 clear_subparser = subparsers.add_parser(
     'clear',
     help='Clear recording data when done with it (does not clear exported video)')
@@ -127,8 +136,11 @@ clear_subparser.add_argument(
     help='Path to recording directory to clear of recorded data')
 clear_subparser.set_defaults(func=clear)
 
+# Parse args
 args = parser.parse_args()
 
+# Call corresponding function to the subcommand that was chosen.
+# AttributeError was being thrown when no subcommand was chosen so in this case print help.
 try:
     args.func(args)
 except AttributeError:
